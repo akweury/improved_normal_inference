84e6a97e8f1b814066da2b29b612622bb8fd5421
Jingyuan Sha <shajingyuan@me.com>
2022-02-06 22:01:30 +0100

    (svd) calculate normal from point clouds

--- pcnormals.py
+++ pcnormals.py
@@ -2,20 +2,35 @@ import os
 from os import path
 import cv2 as cv
 import numpy as np
-from plyfile import PlyData,PlyElement
+from plyfile import PlyData, PlyElement
+from sklearn.neighbors import KDTree
+from sklearn.decomposition import PCA
 
 k = 6
 
-ply_file_path = "./SyntheticDataSet/CapturedData/"
+file_path = "./SyntheticDataSet/CapturedData/"
 file_idx = 0
-file_name = ply_file_path + "0000" + str(file_idx) + ".pointcloud" + str(file_idx) + ".ply"
+file_name = file_path + "0000" + str(file_idx) + ".pointcloud0.ply"
 while path.exists(file_name):
     ply_data = PlyData.read(file_name)
-    vertex = ply_data['vertex']
-    knn = cv.ml.KNearest_create()
-    knn.train(vertex, cv.ml.ROW_SAMPLE)
-    for v in vertex:
-        ret, results, neighbours, dist = knn.findNearest(v,k)
+    vertex = np.c_[ply_data['vertex']['x'], ply_data['vertex']['y'], ply_data['vertex']['z']]
+    dist, ind = KDTree(vertex).query(vertex, k)
+    vertexs_neighbors = vertex[ind]
+    normals = np.zeros(shape=vertex.shape)
+    for i in range(vertexs_neighbors.shape[0]):
+        svd = np.linalg.svd(vertexs_neighbors[i] - np.mean(vertexs_neighbors[i], axis=1, keepdims=True))
+        normals[i] = svd[-1][:, -1] * 0.5 + 0.5
+        normals[i][1] = 1 - normals[i][1]
+        normals[i] = (normals[i] * 255).astype(np.int32)
 
+    normal_image = cv.imread(file_path + "0000" + str(file_idx) + ".normal0.png")
+    normal_idx = 0
+    for i in range(normal_image.shape[0]):
+        for j in range(normal_image.shape[1]):
+            if np.sum(normal_image[i][j]) != 0:
+                normal_image[i][j] = normals[normal_idx]
+                normal_idx += 1
+
+    cv.imwrite(file_path + "0000" + str(file_idx) + ".normal_knn_0.png", normal_image)
     file_idx += 1
-    file_name = ply_file_path + "0000" + str(file_idx) + ".pointcloud" + str(file_idx) + ".ply"
+    file_name = file_path + "0000" + str(file_idx) + ".pointcloud0.ply"
