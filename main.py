cd0d5eaf86f9210d10cfd41a30064d167c3dec9a
Jingyuan Sha <shajingyuan@me.com>
2022-03-01 20:49:55 +0100

    update noise function

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from workspace.pncnn import train
 from workspace import model
 from workspace.pncnn import network
+import config
+from preprocessing.data_preprocess import noisy_a_folder
+for folder in ["selval", "test", "train"]:
+    original_folder = config.synthetic_data / folder
+    noisy_folder = config.synthetic_data_noise / folder
+    noisy_a_folder(original_folder, noisy_folder)
 
 model_param = model.init_env(network)
 
