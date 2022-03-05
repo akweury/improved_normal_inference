29d0f53bc31e94306b6ae44d32145224db6d3318
Jingyuan Sha <shajingyuan@me.com>
2022-03-05 11:11:12 +0100

    update gpu programming

--- workspace/pncnn/network.py
+++ workspace/pncnn/network.py
@@ -38,7 +38,7 @@ class CNN(nn.Module):
 
         # c0 = self.conf_estimator(x0) #  estimate the input confidence
         c0 = mu.binary(x0)
-        xout, cout = self.nconv(x0, c0, cpu=True)  # estimated value of depth
+        xout, cout = self.nconv(x0, c0)  # estimated value of depth
 
         # conv layer with kernal 1
         cout = self.var_estimator(cout)  #
