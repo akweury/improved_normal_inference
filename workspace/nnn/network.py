1d2619f7d696f91530ba70d3e44a229f5328cfd2
Jingyuan Sha <shajingyuan@me.com>
2022-03-08 10:32:17 +0100

    update gpu operation

--- workspace/nnn/network.py
+++ workspace/nnn/network.py
@@ -29,7 +29,7 @@ class CNN(nn.Module):
         # c0: confidence of each element in x0
 
         # c0 = self.conf_estimator(x0) #  estimate the input confidence
-        c0 = mu.binary(x0)
+        c0 = mu.binary(x0).to(x0.get_device())
 
         # out = self.conv(x0)
         out, cout = self.nconv(x0, c0, cpu)  # estimated value of depth
