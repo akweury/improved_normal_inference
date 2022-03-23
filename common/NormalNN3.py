d6f6309402db3691da081c7ff326f66b099b997d
Jingyuan Sha <shajingyuan@me.com>
2022-03-23 21:37:17 +0100

    modify NNN3, add more layers

--- common/NormalNN3.py
+++ common/NormalNN3.py
@@ -22,43 +22,45 @@ class NormalNN3(nn.Module):
         self.active = nn.LeakyReLU(0.1)
         # self.active = nn.Tanh()
         # self.active = nn.ReLU()
-        channel_size_0 = 24
-        channel_size_1 = 72
-        channel_size_2 = 144
-        channel_size_3 = 128
+        channel_size_0 = 32
+        channel_size_1 = 64
+        channel_size_2 = 128
+        channel_size_3 = 256
 
         self.dconv1 = nn.Conv2d(in_ch, channel_size_0, kernel_down, (1, 1), padding_down)
 
-        self.dconv2 = nn.Conv2d(channel_size_0, channel_size_1, kernel_down, (1, 1), padding_down)
+        self.dconv2 = nn.Conv2d(channel_size_0, channel_size_0, kernel_down, (1, 1), padding_down)
 
-        self.dconv3 = nn.Conv2d(channel_size_1, channel_size_1, kernel_down, (1, 1), padding_down)
+        self.dconv3 = nn.Conv2d(channel_size_0, channel_size_1, kernel_down, (1, 1), padding_down)
 
         # self.dconv4 = nn.Conv2d(channel_size_1, channel_size_1, kernel_down, (1, 1), padding_down)
-        #
-        # self.dconv5 = nn.Conv2d(channel_size_1, channel_size_1, kernel_down, (1, 1), padding_down)
-        #
-        # self.dconv6 = nn.Conv2d(channel_size_1, channel_size_1, kernel_down, (1, 1), padding_down)
-        #
-        # self.dconv7 = nn.Conv2d(channel_size_1, channel_size_1, kernel_down, (1, 1), padding_down)
 
-        self.uconv1 = nn.Conv2d(channel_size_2, channel_size_1, kernel_up, (1, 1), padding_up)
+        self.dconv5 = nn.Conv2d(channel_size_1, channel_size_2, kernel_down, (1, 1), padding_down)
+
+        # self.dconv6 = nn.Conv2d(channel_size_2, channel_size_2, kernel_down, (1, 1), padding_down)
+
+        self.dconv7 = nn.Conv2d(channel_size_2, channel_size_2, kernel_down, (1, 1), padding_down)
+
+        # self.dconv8 = nn.Conv2d(channel_size_3, channel_size_2, kernel_down, (1, 1), padding_down)
+
+        self.uconv1 = nn.Conv2d(channel_size_3, channel_size_2, kernel_up, (1, 1), padding_up)
 
         self.uconv2 = nn.Conv2d(channel_size_2, channel_size_1, kernel_up, (1, 1), padding_up)
 
         self.uconv3 = nn.Conv2d(channel_size_2, channel_size_1, kernel_up, (1, 1), padding_up)
 
-        # self.uconv4 = nn.Conv2d(channel_size_2, channel_size_1, kernel_up, (1, 1), padding_up)
-        #
-        # self.uconv5 = nn.Conv2d(channel_size_1, channel_size_1, kernel_up, (1, 1), padding_up)
+        self.uconv4 = nn.Conv2d(channel_size_1, channel_size_0, kernel_up, (1, 1), padding_up)
 
-        self.conv1 = nn.Conv2d(channel_size_1, out_ch, (1, 1), (1, 1), (0, 0))
-        self.conv2 = nn.Conv2d(out_ch, out_ch, (1, 1), (1, 1), (0, 0))
-        # self.conv3 = nn.Conv2d(out_ch, out_ch, (1, 1), (1, 1), (0, 0))
+        self.uconv5 = nn.Conv2d(channel_size_1, channel_size_0, kernel_up, (1, 1), padding_up)
+
+        self.conv1 = nn.Conv2d(channel_size_0, channel_size_0, (1, 1), (1, 1), (0, 0))
+        self.conv2 = nn.Conv2d(channel_size_0, channel_size_0, (1, 1), (1, 1), (0, 0))
+        self.conv3 = nn.Conv2d(channel_size_0, out_ch, (1, 1), (1, 1), (0, 0))
 
     def forward(self, x0):
         x1 = self.active(self.dconv1(x0))  # 512,512
         x1 = self.active(self.dconv2(x1))  # 512,512
-        x1 = self.active(self.dconv3(x1))  # 512,512
+        # x1 = self.active(self.dconv3(x1))  # 512,512
 
         # Downsample 1
         ds = 2
@@ -66,36 +68,41 @@ class NormalNN3(nn.Module):
         x1_ds /= 4
 
         x2_ds = self.active(self.dconv3(x1_ds))  # 256,256
-        x2_ds = self.active(self.dconv3(x2_ds))  # 256,256
+        # x2_ds = self.active(self.dconv4(x2_ds))  # 256,256
 
         # Downsample 2
         ds = 2
         x2_dss, idx = F.max_pool2d(x2_ds, ds, ds, return_indices=True)  # 128,128
         x2_dss /= 4
 
-        x3_ds = self.active(self.dconv3(x2_dss))  # 128,128
+        x3_ds = self.active(self.dconv5(x2_dss))  # 128,128
+        # x3_ds = self.active(self.dconv6(x3_ds))  # 128,128
 
         # Downsample 3
         ds = 2
         x3_dss, idx = F.max_pool2d(x3_ds, ds, ds, return_indices=True)  # 64,64
         x3_dss /= 4
 
-        x4_ds = self.active(self.dconv3(x3_dss))  # 64,64
+        x4_ds = self.active(self.dconv7(x3_dss))  # 64,64
+        # x4_ds = self.active(self.dconv8(x4_ds))  # 64,64
 
         # Upsample 1
         x4 = F.interpolate(x4_ds, x3_ds.size()[2:], mode='nearest')  # 128,128
 
         x34_ds = self.active(self.uconv1(torch.cat((x3_ds, x4), 1)))  # 128, 128
+        x34_ds = self.active(self.uconv2(x34_ds))  # 128, 128
 
         # Upsample 2
         x34 = F.interpolate(x34_ds, x2_ds.size()[2:], mode='nearest')
-        x23_ds = self.active(self.uconv2(torch.cat((x2_ds, x34), 1)))  # 256, 256
+        x23_ds = self.active(self.uconv3(torch.cat((x2_ds, x34), 1)))  # 256, 256
+        x23_ds = self.active(self.uconv4(x23_ds))  # 256, 256
 
         # # Upsample 3
         x23 = F.interpolate(x23_ds, x0.size()[2:], mode='nearest')  # 512, 512
-        xout = self.active(self.uconv3(torch.cat((x23, x1), 1)))  # 512, 512
+        xout = self.active(self.uconv5(torch.cat((x23, x1), 1)))  # 512, 512
 
         # xout = self.active(self.conv1(xout))
         xout = self.active(self.conv1(xout))  # 512, 512
-        xout = self.conv2(xout)
+        xout = self.active(self.conv2(xout))  # 512, 512
+        xout = self.conv3(xout)
         return xout
