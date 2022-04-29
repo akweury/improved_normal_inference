99a8ef4879eb27df233ad3324ae0afe91f578164
Jingyuan Sha <shajingyuan@me.com>
2022-04-29 22:56:52 +0200

    nconv only first 1 layer

--- common/NormalizedNNN.py
+++ common/NormalizedNNN.py
@@ -69,19 +69,19 @@ class NormalizedNNN(nn.Module):
         # c0 = self.c_avg(c0, self.cconv1.weight)
 
         x1 = self.dconv1(x0 * c0)  # 512,512
-        c1 = self.dconv1(c0)
-        x1 = x1 / (c1 + self.epsilon)
-        c1 = self.c_avg(c1, self.dconv1.weight)
+        # c1 = self.dconv1(c0)
+        # x1 = x1 / (c1 + self.epsilon)
+        # c1 = self.c_avg(c1, self.dconv1.weight)
 
-        x1 = self.dconv2(x1 * c1)  # 512,512
-        c1 = self.dconv2(c1)
-        x1 = x1 / (c1 + self.epsilon)
-        c1 = self.c_avg(c1, self.dconv2.weight)
+        x1 = self.dconv2(x1 * c0)  # 512,512
+        # c1 = self.dconv2(c1)
+        # x1 = x1 / (c1 + self.epsilon)
+        # c1 = self.c_avg(c1, self.dconv2.weight)
 
-        x1 = self.dconv3(x1 * c1)  # 512,512
-        c1 = self.dconv3(c1)
-        x1 = x1 / (c1 + self.epsilon)
-        c1 = self.c_avg(c1, self.dconv3.weight)
+        x1 = self.dconv3(x1 * c0)  # 512,512
+        # c1 = self.dconv3(c1)
+        # x1 = x1 / (c1 + self.epsilon)
+        # c1 = self.c_avg(c1, self.dconv3.weight)
 
         # Downsample 1
         ds = 2
@@ -173,4 +173,4 @@ class NormalizedNNN(nn.Module):
         # xout = xout / (cout + self.epsilon)
         # cout = self.c_avg(cout, self.conv1.weight)
         # xout = self.conv2(xout)
-        return x7, c1
+        return x7, c0
