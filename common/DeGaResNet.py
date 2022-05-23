8f2153033dcf698dc6bd8b6cbb7c4e208d4d6870
Jingyuan Sha <shajingyuan@me.com>
2022-05-23 19:01:25 +0200

    reduce model size

--- common/DeGaResNet.py
+++ common/DeGaResNet.py
@@ -37,12 +37,11 @@ class DeGaResNet(nn.Module):
         self.active_img = nn.LeakyReLU(0.01)
 
         self.epsilon = 1e-20
-        channel_size_1 = 32
-        channel_size_2 = 64
+        channel_size_1 = 16
+        channel_size_2 = 32
         # https://homepages.inf.ed.ac.uk/rbf/CVonline/LOCAL_COPIES/PIRODDI1/NormConv/node2.html#:~:text=The%20idea%20of%20normalized%20convolution,them%20is%20equal%20to%20zero.
 
         self.resnet50 = ResNet(Bottleneck, layers=[1, 3, 4, 5])
-        self.edgeNet = UNet(3, 3)
         # branch 1
         self.dconv1 = GConv(in_ch, channel_size_1, kernel_down, stride, padding_down)
         self.dconv2 = GConv(channel_size_1, channel_size_1, kernel_down, stride, padding_down)
