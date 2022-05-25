afd3aa51dd17b9db70a0494935879316159d8272
Jingyuan Sha <shajingyuan@me.com>
2022-05-25 11:22:06 +0200

    print cuda info

--- help_funs/worksheet.py
+++ help_funs/worksheet.py
@@ -1,25 +1,22 @@
 import numpy as np
 import torch
 import cv2 as cv
-import config
-from help_funs import file_io, mu, chart
-from pncnn.utils import args_parser
 
 
-def test_torch():
-    # depth_file = str(config.ws_path / "a.png")
-    data_file = str(config.synthetic_data / "test" / '00327.normal0.png')
-    normal = file_io.load_24bitNormal(data_file).astype(np.float32)
-    data_tensor = torch.from_numpy(normal).permute(2, 0, 1)
-    img = mu.hpf_torch(data_tensor)
-    cv.imwrite(str(config.ws_path / f"aa.png"), img.numpy())
-
-
-def test_png():
-    depth_file = str(
-        config.ws_path / "degares" / "output" / "output_2022-05-24_15_14_04" / "train_epoch_199_0_loss_0.16585270.png")
-    img = mu.hpf(depth_file)
-    cv.imwrite(str(config.ws_path / "degares" / "output" / "output_2022-05-24_15_14_04" / f"aa.png"), img)
+# def test_torch():
+#     # depth_file = str(config.ws_path / "a.png")
+#     data_file = str(config.synthetic_data / "test" / '00327.normal0.png')
+#     normal = file_io.load_24bitNormal(data_file).astype(np.float32)
+#     data_tensor = torch.from_numpy(normal).permute(2, 0, 1)
+#     img = mu.hpf_torch(data_tensor)
+#     cv.imwrite(str(config.ws_path / f"aa.png"), img.numpy())
+#
+#
+# def test_png():
+#     depth_file = str(
+#         config.ws_path / "degares" / "output" / "output_2022-05-24_15_14_04" / "train_epoch_199_0_loss_0.16585270.png")
+#     img = mu.hpf(depth_file)
+#     cv.imwrite(str(config.ws_path / "degares" / "output" / "output_2022-05-24_15_14_04" / f"aa.png"), img)
 
 
 def print_cuda_info():
