ec07420822c5e76b4186e9c1af78ce81132ce7c7
Jingyuan Sha <shajingyuan@me.com>
2022-05-31 19:46:31 +0200

    add more layers in model ag

--- workspace/ag/network.py
+++ workspace/ag/network.py
@@ -15,13 +15,19 @@ class CNN(nn.Module):
         self.__name__ = 'ag'
 
         self.agconv3_3 = AlbedoGatedNNN(3, 3)
-        self.active = nn.LeakyReLU(0.01)
-        self.albedoInpainting1 = GConv(1, 1, (3, 3), (1, 1), (1, 1))
-        self.albedoInpainting2 = GConv(1, 1, (3, 3), (1, 1), (1, 1))
-        self.albedoInpainting3 = nn.Conv2d(1, 1, (3, 3), (1, 1), (1, 1))
+        self.active_leaky_relu = nn.LeakyReLU(0.01)
+        self.active_sigmoid = nn.Sigmoid()
+
         self.lsInpainting1 = GConv(3, 3, (3, 3), (1, 1), (1, 1))
-        self.lsInpainting2 = GConv(3, 3, (3, 3), (1, 1), (1, 1))
+        self.lsInpainting2 = nn.Conv2d(3, 3, (3, 3), (1, 1), (1, 1))
         self.lsInpainting3 = nn.Conv2d(3, 3, (3, 3), (1, 1), (1, 1))
+
+        self.nl_layer = nn.Conv2d(6, 1, (3, 3), (1, 1), (1, 1))
+        self.rho_layer = GConv(2, 1, (3, 3), (1, 1), (1, 1))
+
+        self.albedoInpainting1 = nn.Conv2d(1, 1, (3, 3), (1, 1), (1, 1))
+        self.albedoInpainting2 = nn.Conv2d(1, 1, (3, 3), (1, 1), (1, 1))
+        self.albedoInpainting3 = nn.Conv2d(1, 1, (3, 3), (1, 1), (1, 1))
 
     def forward(self, xin):
         x0 = xin[:, :3, :, :]
@@ -33,16 +39,17 @@ class CNN(nn.Module):
 
         # light source inpainting
         L = self.lsInpainting1(light_direction)
-        L = self.lsInpainting2(L)
-        L = self.active(self.lsInpainting3(L))
+        L = self.active_leaky_relu(self.lsInpainting2(L))
+        L = self.active_leaky_relu(self.lsInpainting3(L))
 
-        # rho inpainting
-        G = torch.abs(torch.sum(x_normal_out * L, dim=1, keepdim=True))
-        rho = x_img0 / (G + 1e-20)
-        rho = rho / rho.max()
-        rho = self.albedoInpainting1(rho)
-        rho = self.albedoInpainting2(rho)
-        x_rho_out = self.active(self.albedoInpainting3(rho))
+        # G = torch.abs(torch.sum(x_normal_out * L, dim=1, keepdim=True))
+        G = self.active_leaky_relu(self.nl_layer(torch.cat((L, x_normal_out), 1)))
+
+        # rho = (x_img0 / (G + 1e-20)) / (x_img0 / (G + 1e-20)).max()
+        rho = self.active_leaky_relu(self.rho_layer(torch.cat((G, x_img0), 1)))
+        rho = self.active_leaky_relu(self.albedoInpainting1(rho))
+        rho = self.active_leaky_relu(self.albedoInpainting2(rho))
+        x_rho_out = self.albedoInpainting3(rho)
 
         out = torch.cat((x_normal_out, x_rho_out, L), 1)
         return out
