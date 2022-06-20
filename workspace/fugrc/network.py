e783e912e6d9132a02e29c07550839c346655733
Jingyuan Sha <shajingyuan@me.com>
2022-06-20 23:37:37 +0200

    test for fugrc pipeline

--- workspace/fugrc/network.py
+++ workspace/fugrc/network.py
@@ -5,6 +5,7 @@ sys.path.append(dirname(__file__))
 
 import torch.nn as nn
 from common.FGCNN import FGCNN
+from common.NormalizedNNN import NormalizedNNN
 
 
 class CNN(nn.Module):
@@ -13,7 +14,8 @@ class CNN(nn.Module):
         self.__name__ = 'fugrc'
 
         # input confidence estimation network
-        self.net3_3 = FGCNN(3, 3, channel_num)
+        # self.net3_3 = FGCNN(3, 3, channel_num)
+        self.net3_3 = NormalizedNNN(3, 3, channel_num)
 
     def forward(self, x0):
         # x0: vertex array
