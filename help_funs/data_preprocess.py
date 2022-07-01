1a34bb96584beeb118a9010afd49152f20595877
Jingyuan Sha <shajingyuan@me.com>
2022-07-01 15:55:26 +0200

    fix a bug

--- help_funs/data_preprocess.py
+++ help_funs/data_preprocess.py
@@ -61,7 +61,7 @@ def noisy_a_folder(folder_path, output_path):
     depth_files = np.array(sorted(glob.glob(str(folder_path / "*depth0*.png"), recursive=True)))
     data_files = np.array(sorted(glob.glob(str(folder_path / "*data0*.json"), recursive=True)))
     img_files = np.array(sorted(glob.glob(str(folder_path / "*image0*.png"), recursive=True)))
-    for idx in range(len(img_files)):
+    for idx in range(len(data_files)):
         if os.path.exists(str(output_path / (str(idx).zfill(5) + ".depth0.png"))):
             continue
         if os.path.exists(data_files[idx]):
