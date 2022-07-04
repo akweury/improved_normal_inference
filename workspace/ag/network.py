f7e5431406b0d670b657597cb1c09c84bb07295b
Jingyuan Sha <shajingyuan@me.com>
2022-07-04 13:47:54 +0200

    update ag model

--- workspace/ag/network.py
+++ workspace/ag/network.py
@@ -6,6 +6,7 @@ import torch
 import torch.nn as nn
 from common.Layers import GConv, LightNet
 from common.ResNormalGuided import NormalGuided
+import config
 
 
 # from common.NormalizedNNN import NormalizedNNN
