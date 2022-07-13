7a233a5881958da851e253fe159cd6e6cda6135e
Jingyuan Sha <shajingyuan@me.com>
2022-07-13 17:46:26 +0200

    config dfki server

--- workspace/train.py
+++ workspace/train.py
@@ -108,7 +108,7 @@ class TrainingModel():
         self.missing_keys = None
         self.args = args
         self.start_epoch = start_epoch
-        self.device = torch.device(device_name)
+        self.device = torch.device(0)
         self.exp_name = self.args.exp
         self.exp_dir = Path(exp_dir)
         self.output_folder = self.init_output_folder()
