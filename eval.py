f96abdbf3d7b94b86c998b57ece89c398874eb4b
Jingyuan Sha <shajingyuan@me.com>
2022-07-25 23:58:23 +0200

    update eval

--- eval.py
+++ eval.py
@@ -87,18 +87,18 @@ if __name__ == '__main__':
 
     models = {
         # test
-        # "an2-8-1000": config.paper_exp / "an2" / "checkpoint-8-1000-208.pth.tar",
-        # "an3-8-1000": config.paper_exp / "an3" / "checkpoint-8-1000-469.pth.tar",
-        # "vil-8-1000": config.paper_exp / "vil10" / "checkpoint-8-1000-779.pth.tar",
-        # "vil10-8-1000": config.paper_exp / "vil10" / "checkpoint-10-8-1000-170.pth.tar",
-        # "GCNN-8-1000": config.paper_exp / "gcnn" / "checkpoint-8-1000-819.pth.tar",
-        "GCNN-8-1000": config.paper_exp / "gcnn" / "checkpoint-8-1000-1099.pth.tar",
+        "an-8-1000": config.paper_exp / "an" / "checkpoint-239.pth.tar",
+        # "an3-8-1000": config.paper_exp / "an3" / "checkpoint-8-1000-692.pth.tar",
+        # "vil-8-1000": config.paper_exp / "vil10" / "checkpoint-8-1000-1256.pth.tar",
+        # "vil10-8-1000": config.paper_exp / "vil10" / "checkpoint-10-8-1000-209.pth.tar",
 
         # record
+        # "an2-8-1000": config.paper_exp / "an2" / "checkpoint-8-1000-432.pth.tar",
         # "an3-3-12-1000": config.paper_exp / "an3" / "checkpoint-3-12-1000-629.pth.tar",
         # "vil-10-1000": config.paper_exp / "vil10" / "checkpoint-10-1000.pth.tar",
-        # "GCNN-3-12-1000": config.paper_exp / "gcnn" / "checkpoint-3-12-1000.pth.tar",
-        "SVD": None,
+        # "GCNN-8-1000": config.paper_exp / "gcnn" / "checkpoint-8-1000-1099.pth.tar",
+
+        # "SVD": None,
 
     }
     test_folder = config.synthetic_data_noise_local / "synthetic128"
