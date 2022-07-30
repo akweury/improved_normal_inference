d1774eb6bf35d14364e17b5dd9ec7087550ffa3c
Jingyuan Sha <shajingyuan@me.com>
2022-07-30 17:57:54 +0200

    update nnnn model

--- README.md
+++ README.md
@@ -88,7 +88,7 @@ CUDA_VISIBLE_DEVICES=2 python3 main.py --machine remote --exp albedoGated --data
 
     
     srun \
-      --job-name="INI-an2-512" \
+      --job-name="INI-an2" \
       --time=7-00:00 \
       -p RTXA6000 \
       --ntasks=1 \
@@ -98,7 +98,7 @@ CUDA_VISIBLE_DEVICES=2 python3 main.py --machine remote --exp albedoGated --data
       --container-image=/netscratch/enroot/nvcr.io_nvidia_pytorch_21.08-py3.sqsh \
       --container-workdir="`pwd`" \
       --container-mounts=/netscratch/$USER:/netscratch/$USER,/ds:/ds:ro,"`pwd`":"`pwd`" \
-      python3 main.py --machine remote --exp an2 --dataset synthetic512 --batch_size 2 --lightNumUse 1 --lr-scheduler 8,1000 --resume /home/sha/improved_normal_inference/workspace/an2/output_2022-07-27_21_31_59/checkpoint-303.pth.tar 
+      python3 main.py --machine remote --exp an2 --dataset synthetic512 --batch_size 8 --lightNumUse 1 --lr-scheduler 8,1000 --resume /home/sha/improved_normal_inference/workspace/an2/output_2022-07-27_21_31_59/checkpoint-303.pth.tar 
 
 
     srun \
--- help_funs/visual_loss_from_multi_model.py
+++ help_funs/visual_loss_from_multi_model.py
@@ -6,10 +6,14 @@ from help_funs import chart
 
 models = {
     # "GCNN-1534": config.ws_path / "nnnn" / "trained_model" / "128" / "checkpoint-c32.pth.tar",
-    "vil10-100-1000": config.ws_path / "vil10" / "trained_model" / "128" / "checkpoint-l10-100-1000.pth.tar",
-    "vil3-100-1000": config.ws_path / "vil10" / "trained_model" / "128" / "checkpoint-l3-100-1000.pth.tar",
-    "vil-3-12-1000": config.ws_path / "vil10" / "trained_model" / "128" / "checkpoint-l1-3-12-1000.pth.tar",
-    "vil-10-1000": config.ws_path / "vil10" / "trained_model" / "128" / "checkpoint-l1-10-1000.pth.tar",
+    # "vil10-100-1000": config.ws_path / "vil10" / "trained_model" / "128" / "checkpoint-l10-100-1000.pth.tar",
+    # "vil3-100-1000": config.ws_path / "vil10" / "trained_model" / "128" / "checkpoint-l3-100-1000.pth.tar",
+    # "vil-3-12-1000": config.ws_path / "vil10" / "trained_model" / "128" / "checkpoint-l1-3-12-1000.pth.tar",
+    # "vil-10-1000": config.ws_path / "vil10" / "trained_model" / "128" / "checkpoint-l1-10-1000.pth.tar",
+
+    "GCNN": config.paper_exp / "gcnn" / "checkpoint-gcnn-1099.pth.tar",  # GCNN
+    "NOC": config.paper_exp / "gcnn" / "checkpoint-noc-807.pth.tar",
+    "CNN": config.paper_exp / "gcnn" / "checkpoint-cnn-695.pth.tar",
 
 }
 
@@ -22,5 +26,8 @@ for model_idx, (name, model) in enumerate(models.items()):
     batch_size = args.batch_size
     loss = checkpoint['losses'] / batch_size
     normal_loss_avg = np.sum(loss[:3], axis=0, keepdims=True) / 3
+    for idx in range(normal_loss_avg.shape[1]):
+        if normal_loss_avg[0, idx] == 0:
+            normal_loss_avg[0, idx] = normal_loss_avg[0, idx - 1]
     chart.line_chart(np.array(normal_loss_avg), output_folder, labels=[name], title="Loss Comparison",
-                     y_label="L2 Loss", log_y=True)
+                     y_label="L2 Loss", log_y=False)
