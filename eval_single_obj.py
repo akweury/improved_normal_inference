b9f325cfbe59f46d3edf245ae21c3d3ae968cf96
Jingyuan Sha <shajingyuan@me.com>
2022-08-01 19:33:37 +0200

    update eval

--- eval_single_obj.py
+++ eval_single_obj.py
@@ -130,8 +130,10 @@ if __name__ == '__main__':
                 test_folder = config.real_data_dfki / "test"
             models = {
                 # "SVD": None,
-                "GCNN-512-269": config.model_dfki / "checkpoint-289.pth.tar",  # GCNN
+                "GCNN-512-289": config.model_dfki / "checkpoint-289.pth.tar",  # GCNN
+                "GCNN-512-269": config.model_dfki / "checkpoint-269.pth.tar",  # GCNN
                 "Trip-Net-512": config.model_dfki / "checkpoint-68.pth.tar",  # GCNN
+                "Trip-Net-512-2": config.model_dfki / "checkpoint-32.pth.tar",  # GCNN
                 # "Trip-Net-Finetune": config.model_dfki / "checkpoint-11.pth.tar",  # GCNN
             }
 
