76297f56670dd2f4a0a7b9e09a801ceb3aa26a3e
Jingyuan Sha <shajingyuan@me.com>
2022-08-05 18:03:25 +0200

    update paper

--- workspace/nnnn/eval_feature_maps.py
+++ workspace/nnnn/eval_feature_maps.py
@@ -9,8 +9,8 @@ from eval_visual import preprocessing, evaluate_epoch
 from help_funs import mu
 
 models = {
-    # "gcnn-cnn": config.gcnn_cnn,
-    # "gcnn-noc": config.gcnn_noc,
+    "gcnn-cnn": config.gcnn_cnn,
+    "gcnn-noc": config.gcnn_noc,
     "gcnn-gcnn": config.gcnn_gcnn,
 }
 
@@ -31,14 +31,16 @@ light_gt = gt_tensor[:, 13:16, :, :].permute(2, 3, 1, 0).squeeze(-1).numpy()
 light_input = test_0_tensor[:, 13:16, :, :].permute(2, 3, 1, 0).squeeze(-1).numpy()
 vertex_input = test_0_tensor[:, :3, :, :].permute(2, 3, 1, 0).squeeze(-1).numpy()
 img_gt = test_0_tensor[:, 3:4, :, :].permute(2, 3, 1, 0).squeeze(-1).numpy()
-
+gt_resized = mu.image_resize(gt, 512, 512)
 mask = gt.sum(axis=2) == 0
+mask_512 = gt_resized.sum(axis=2) == 0
 
 img_8bit = cv.normalize(np.uint8(img_gt), None, 0, 255, cv.NORM_MINMAX, dtype=cv.CV_8U)
 img = cv.merge((img_8bit, img_8bit, img_8bit))
 normal_no_mask_img = None
 FEATURE_MAP_NUM = 128
-CV_COLOR = cv.COLORMAP_TURBO
+# CV_COLOR = cv.COLORMAP_TURBO
+CV_COLOR = cv.COLORMAP_DEEPGREEN
 
 # evaluate CNN models
 for model_idx, (name, model) in enumerate(models.items()):
@@ -120,25 +122,44 @@ for model_idx, (name, model) in enumerate(models.items()):
     act = activation['dconv4'].squeeze().to("cpu")
     for idx in range(FEATURE_MAP_NUM):
         feature_map = act[idx].unsqueeze(-1).numpy()
+
+        feature_map = (feature_map - feature_map.min()) / (feature_map.max() - feature_map.min())
+
         feature_map = mu.visual_vertex(feature_map, str(idx))
+        feature_map[mask_512] = 0
+
+        feature_map[0, :] = 255
+        feature_map[-1, :] = 255
+        feature_map[:, 0] = 255
+        feature_map[:, -1] = 255
         feature_map_dconv4.append(cv.applyColorMap(feature_map, CV_COLOR))
 
     act = activation['uconv1'].squeeze().to("cpu")
     for idx in range(FEATURE_MAP_NUM):
         feature_map = act[idx].unsqueeze(-1).numpy()
+        feature_map = (feature_map - feature_map.min()) / (feature_map.max() - feature_map.min())
         feature_map = mu.visual_vertex(feature_map, str(idx))
+        feature_map[mask_512] = 0
+        feature_map[0, :] = 255
+        feature_map[-1, :] = 255
+        feature_map[:, 0] = 255
+        feature_map[:, -1] = 255
         feature_map_uconv1.append(cv.applyColorMap(feature_map, CV_COLOR))
 
     act = activation['uconv2'].squeeze().to("cpu")
     for idx in range(FEATURE_MAP_NUM):
         feature_map = act[idx].unsqueeze(-1).numpy()
+        feature_map = (feature_map - feature_map.min()) / (feature_map.max() - feature_map.min())
         feature_map = mu.visual_vertex(feature_map, str(idx))
+        feature_map[mask_512] = 0
         feature_map_uconv2.append(cv.applyColorMap(feature_map, CV_COLOR))
 
     act = activation['uconv3'].squeeze().to("cpu")
     for idx in range(FEATURE_MAP_NUM):
         feature_map = act[idx].unsqueeze(-1).numpy()
+        feature_map = (feature_map - feature_map.min()) / (feature_map.max() - feature_map.min())
         feature_map = mu.visual_vertex(feature_map, str(idx))
+        feature_map[mask_512] = 0
         feature_map_uconv3.append(cv.applyColorMap(feature_map, CV_COLOR))
 
     from itertools import islice
@@ -149,7 +170,8 @@ for model_idx, (name, model) in enumerate(models.items()):
         return [list(islice(it, i)) for i in var_lst]
 
 
-    feature_map_list = convert(feature_map_uconv3, [16, 16, 16, 16, 16, 16, 16, 16])
+    feature_map_list = convert(feature_map_uconv1, [16, 16, 16, 16, 16, 16, 16, 16])
+    # feature_map_list = convert(feature_map_dconv4, [16, 16, 16, 16, 16, 16, 16, 16])
 
     # feature_map_list = [
     #     # feature_map_dconv1, feature_map_dconv2, feature_map_dconv3, feature_map_dconv4,
